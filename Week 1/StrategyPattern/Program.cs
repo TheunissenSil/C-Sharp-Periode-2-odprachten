c4827df0147d921788549e79d6560905ef099f4d
TheunissenSil <theunissensil@gmail.com>
2023-11-23 10:42:40 +0100

    Opdracht 1 en 2

---
+++ Week 1/StrategyPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using StrategyPattern.Ducks;
+using StrategyPattern.Interfaces.FlyBehavior;
+
+namespace StrategyPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallardDuck = new MallardDuck();
+            Duck redheadDuck = new RedheadDuck();
+            Duck decoyDuck = new DecoyDuck();
+            Duck rubberDuck = new RubberDuck();
+            
+            mallardDuck.Display();
+            redheadDuck.Display();
+            decoyDuck.Display();
+            rubberDuck.Display();
+        }
+    }
+}
