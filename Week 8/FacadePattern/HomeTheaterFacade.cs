5fa6d91db2a5934bf0f83345dce317509b5982ef
TheunissenSil <theunissensil@gmail.com>
2024-01-23 10:33:38 +0100

    Opdracht 1 & 2

---
+++ Week 8/FacadePattern/HomeTheaterFacade.cs
@@ -0,0 +1,67 @@
+﻿namespace FacadePattern
+{
+    internal class HomeTheaterFacade
+    {
+        private Amplifier _amp;
+        private CdPlayer _cdPlayer;
+        private DvdPlayer _dvdPlayer;
+        private PopcornPopper _popcornPopper;
+        private Projector _projector;
+        private Screen _screen;
+        private TheaterLights _lights;
+        private Tuner _tuner;
+
+        public HomeTheaterFacade(
+            Amplifier amp,
+            CdPlayer cdPlayer,
+            DvdPlayer dvdPlayer,
+            PopcornPopper popcornPopper,
+            Projector projector,
+            Screen screen,
+            TheaterLights lights,
+            Tuner tuner)
+        {
+            _amp = amp;
+            _cdPlayer = cdPlayer;
+            _dvdPlayer = dvdPlayer;
+            _popcornPopper = popcornPopper;
+            _projector = projector;
+            _screen = screen;
+            _lights = lights;
+            _tuner = tuner;
+        }
+
+        // Implement the WatchMovie method
+        public void WatchMovie(string movie)
+        {
+            Console.WriteLine($"Get ready to watch a movie: {movie}");
+            _popcornPopper.On();
+            _popcornPopper.Pop();
+            _lights.Dim(10);
+            _screen.Down();
+            _projector.On();
+            _projector.SetInput(_dvdPlayer);
+            _projector.WideScreenMode();
+            _amp.On();
+            _amp.SetDvd(_dvdPlayer);
+            _amp.SetSurroundSound();
+            _amp.SetVolume(5);
+            _dvdPlayer.On();
+            _dvdPlayer.Play(movie);
+        }
+
+        // Implement the EndMovie method
+        public void EndMovie()
+        {
+            Console.WriteLine("Shutting down the movie theater");
+            _popcornPopper.Off();
+            _lights.On();
+            _screen.Up();
+            _projector.Off();
+            _amp.Off();
+            _dvdPlayer.Stop();
+            _dvdPlayer.Eject();
+            _dvdPlayer.Off();
+        }
+    }
+}
---
+++ Week 8/FacadePattern/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FacadePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Amplifier amp = new Amplifier();
+            CdPlayer cdPlayer = new CdPlayer(amp);
+            DvdPlayer dvdPlayer = new DvdPlayer(amp);
+            PopcornPopper popcornPopper = new PopcornPopper();
+            Projector projector = new Projector();
+            Screen screen = new Screen();
+            TheaterLights lights = new TheaterLights();
+            Tuner tuner = new Tuner(amp);
+
+            // Create the HomeTheaterFacade instance
+            HomeTheaterFacade homeTheater = new HomeTheaterFacade(amp, cdPlayer, dvdPlayer, popcornPopper, projector, screen, lights, tuner);
+
+            // Start watching a movie
+            homeTheater.WatchMovie("Die Hard");
+
+            // End the movie
+            homeTheater.EndMovie();
+        }
+    }
+}
